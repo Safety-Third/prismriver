37e2be7a0188e991827be5da0e30787c42f4f3de
Alice W. Nguyen <alice.nguyen@ttp.codes>
2021-09-13 01:27:51 -0400

    web: set title

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  // yes, all of this is needed because vue-cli apparently overlooked all of this
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      title: '2E Music Memes'
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
