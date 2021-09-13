b6899e9e7ecbaed6f7620c057a438597310377c7
Alice W. Nguyen <alice.nguyen@ttp.codes>
2021-09-13 01:22:08 -0400

    web: set app colors

--- web/src/plugins/vuetify.ts
+++ web/src/plugins/vuetify.ts
@@ -1,7 +1,17 @@
 import Vue from 'vue'
+import { colors } from 'vuetify/lib'
 import Vuetify from 'vuetify/lib/framework'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.deepOrange,
+        secondary: colors.deepOrange.lighten4,
+        accent: colors.deepOrange.accent1
+      }
+    }
+  }
 })
