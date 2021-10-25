7d6df2f53316260c908b7d61ae5b2e28e8aaa301
Alice W. Nguyen <alice.nguyen@ttp.codes>
2021-10-25 18:25:03 -0400

    server: catch SIGINT instead of SIGKILL

--- internal/app/server/router.go
+++ internal/app/server/router.go
@@ -47,7 +47,7 @@ func CreateRouter() {
 	logrus.Info("HTTP server now listening on port 8000.")
 
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Kill)
+	signal.Notify(c, os.Interrupt)
 
 	<-c
 
