f05deee83e1470df35d53ff9454d5440304030b1
Alice W. Nguyen <alice.nguyen@ttp.codes>
2021-10-25 20:56:56 -0400

    web: fix websocket path configuration

--- web/src/plugins/websocket.ts
+++ web/src/plugins/websocket.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 
-const websocketBase = process.env.NODE_ENV === 'production' ? process.env.WS_URL || `${window.location.protocol ===
+const websocketBase = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_WS_URL || `${window.location.protocol ===
   'https:' ? 'wss:' : 'ws:'}//${window.location.hostname}/` : 'ws://localhost:8000/'
 
 Vue.prototype.$websocket = (path: string) => {
