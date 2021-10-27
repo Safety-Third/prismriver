1a7b9c9b4a983207302da9f3f08ec749abc513dc
Alice W. Nguyen <alice.nguyen@ttp.codes>
2021-10-27 15:01:11 -0400

    web: replace queueitem buttons on desktop

--- web/src/components/QueueItem.vue
+++ web/src/components/QueueItem.vue
@@ -13,11 +13,24 @@
         <ProgressBar :error="error" :progress="progress"/>
       </v-list-item-subtitle>
     </v-list-item-content>
-    <v-list-item-action class="my-0 py-0 ml-8">
-      <v-btn depressed small color="deep-orange accent-1" :disabled="disabledown" @click="down"><v-icon>mdi-arrow-down-bold</v-icon></v-btn>
+    <v-list-item-action class="my-0 py-0 ml-4" v-if="$vuetify.breakpoint.xs">
+      <v-btn depressed small color="deep-orange accent-1" :disabled="disabledown" @click="move('down')"><v-icon>mdi-arrow-down-bold</v-icon></v-btn>
     </v-list-item-action>
-    <v-list-item-action class="my-0 py-0">
-      <v-btn depressed small color="deep-orange accent-1" :disabled="disableup" @click="up"><v-icon>mdi-arrow-up-bold</v-icon></v-btn>
+    <v-list-item-action class="my-0 py-0" v-if="$vuetify.breakpoint.xs">
+      <v-btn depressed small color="deep-orange accent-1" :disabled="disableup" @click="move('up')"><v-icon>mdi-arrow-up-bold</v-icon></v-btn>
+    </v-list-item-action>
+    <v-list-item-action class="ml-4 my-0 py-0" v-else>
+      <v-menu :disabled="disableup && disabledown">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn plain small text :ripple="false" v-bind="attrs" v-on="on" :disabled="disableup && disabledown"><v-icon>mdi-dots-vertical</v-icon></v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-if="!disableup" dense @click="move('top')"><v-list-item-title><v-icon>mdi-arrow-collapse-up</v-icon> Move to Top</v-list-item-title></v-list-item>
+          <v-list-item v-if="!disableup" dense @click="move('up')"><v-list-item-title><v-icon>mdi-arrow-up</v-icon> Move Up</v-list-item-title></v-list-item>
+          <v-list-item v-if="!disabledown" dense @click="move('down')"><v-list-item-title><v-icon>mdi-arrow-down</v-icon> Move Down</v-list-item-title></v-list-item>
+          <v-list-item v-if="!disabledown" dense @click="move('bottom')"><v-list-item-title><v-icon>mdi-arrow-collapse-down</v-icon> Move to Bottom</v-list-item-title></v-list-item>
+        </v-list>
+      </v-menu>
     </v-list-item-action>
   </v-list-item>
 </template>
@@ -54,14 +67,9 @@ export default Vue.extend({
     deleteSong () {
       this.$http.delete(`queue/${this.index}`)
     },
-    down () {
+    move (to: string) {
       this.$http.put(`queue/${this.index}`, new URLSearchParams({
-        move: 'down'
-      }))
-    },
-    up () {
-      this.$http.put(`queue/${this.index}`, new URLSearchParams({
-        move: 'up'
+        move: to
       }))
     }
   },
