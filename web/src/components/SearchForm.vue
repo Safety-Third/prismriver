43ca2a9688480b7f13b38479ba29aa4cdf491198
Alice W. Nguyen <alice.nguyen@ttp.codes>
2021-11-01 14:37:20 -0400

    web: support media search paging

--- web/src/components/SearchForm.vue
+++ web/src/components/SearchForm.vue
@@ -6,12 +6,16 @@
         <v-icon v-if="show">mdi-chevron-up</v-icon>
         <v-icon v-else>mdi-chevron-down</v-icon>
       </v-btn>
+      <v-spacer/>
+      <v-btn depressed small color="deep-orange accent-1" v-if="show" :disabled="[0, 1].indexOf(page) !== -1" @click="toPage(page - 1)"><v-icon>mdi-arrow-left-bold</v-icon></v-btn>
+      <span class="mx-2 text-subtitle-1" v-if="show">{{ page }} / {{ pages }}</span>
+      <v-btn depressed small color="deep-orange accent-1" v-if="show" :disabled="page == pages" @click="toPage(page + 1)"><v-icon>mdi-arrow-right-bold</v-icon></v-btn>
     </v-card-title>
     <v-expand-transition>
       <div v-show="show">
         <v-card-text class="align-center d-flex my-0 py-0">
           <v-col class="mx-0 px-0 my-0 py-0" :cols="$vuetify.breakpoint.xs ? 6 : 8">
-            <v-text-field class="my-0 py-0" dense hide-details outlined label="Search" v-model="query" @keydown.enter.prevent="submit"/>
+            <v-text-field class="my-0 py-0" dense hide-details outlined label="Search" v-model="query" @keydown.enter.prevent="submit" @input="page = pages = 1"/>
           </v-col>
           <v-col class="mx-0 px-2 my-0 py-0" :cols="$vuetify.breakpoint.xs ? 3 : 2">
             <v-btn class="my-0 py-0 text-none text-h6" color="deep-orange accent-1" width="100%" depressed @click="submit">Search</v-btn>
@@ -52,6 +56,8 @@ export default Vue.extend({
   },
 
   data: () => ({
+    page: 1,
+    pages: 1,
     query: '',
     results: [],
     show: false
@@ -59,22 +65,35 @@ export default Vue.extend({
 
   methods: {
     async getRandomSearch () {
-      this.results = (await this.$http.get('media/random', {
-        params: {
-          limit: 12
-        }
-      })).data
+      const data = (await this.$http.get('media')).data
+      this.results = data.media
+      this.page = Math.min(1, data.pages)
+      this.pages = data.pages
     },
     async submit (): Promise<void> {
       if (this.query.length) {
-        this.results = (await this.$http.get('media/search', {
+        const data = (await this.$http.get('media', {
           params: {
             query: this.query
           }
         })).data
+        this.results = data.media
+        this.page = Math.min(1, data.pages)
+        this.pages = data.pages
       } else {
         this.getRandomSearch()
       }
+    },
+    async toPage (page: number) {
+      this.page = page
+      const data = (await this.$http.get('media', {
+        params: {
+          page: this.page,
+          query: this.query
+        }
+      })).data
+      this.results = data.media
+      this.pages = data.pages
     }
   },
 
