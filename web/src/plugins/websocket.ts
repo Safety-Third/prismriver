4f900b653590f3f58076e4a6cf93f3d5bed1141e
Alice W. Nguyen <alice.nguyen@ttp.codes>
2021-11-01 14:32:21 -0400

    web: remove need for trailing separator for ws

--- web/src/plugins/websocket.ts
+++ web/src/plugins/websocket.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 
 const websocketBase = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_WS_URL || `${window.location.protocol ===
-  'https:' ? 'wss:' : 'ws:'}//${window.location.hostname}/` : 'ws://localhost:8000/'
+  'https:' ? 'wss:' : 'ws:'}//${window.location.hostname}` : 'ws://localhost:8000'
 
 Vue.prototype.$websocket = (path: string) => {
-  return new WebSocket(`${websocketBase}${path}`)
+  return new WebSocket(`${websocketBase}/${path}`)
 }
