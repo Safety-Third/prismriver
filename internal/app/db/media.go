f10201452688f08c860270a105ac2e5dc1ead451
Alice W. Nguyen <alice.nguyen@ttp.codes>
2021-11-04 22:19:34 -0400

    media: remove deletedat field

--- internal/app/db/media.go
+++ internal/app/db/media.go
@@ -11,7 +11,6 @@ type Media struct {
 	ID        string `gorm:"primary_key"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	DeletedAt *time.Time `sql:"index"`
 
 	Length uint64 `gorm:"not null"`
 	Title  string `gorm:"not null"`
